26daa319d6ce4a18a5c83f5e17426563f10ccc93
GreedNeSS <ruslan199126@gmail.com>
2022-05-03 20:36:22 +0300

    Added configuration basic example.

--- ConfigurationBasics/Program.cs
+++ ConfigurationBasics/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddInMemoryCollection(new Dictionary<string, string>
+{
+    { "token", "214365" },
+    { "port", "8080" },
+});
 var app = builder.Build();
+app.Configuration["UserName"] = "Greed";
+app.Configuration["UserAge"] = "30";
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (IConfiguration appConfig) => $"Token: {appConfig["token"]}, Port: {appConfig["port"]}");
+app.MapGet("/user", (IConfiguration appConfig) => new {Name = appConfig["UserName"],
+    Age = int.Parse(appConfig.GetSection("UserAge").Value)});
 
 app.Run();
